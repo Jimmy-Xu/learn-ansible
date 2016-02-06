80f3c8b135cfda57e505aa9498062a06e2b50e23
Jimmy Xu <xjimmyshcn@gmail.com>
2016-02-06 19:56:46 +0000

    optimize

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-case "$1" in
-  centos7)  ;;
-  fedora23) ;;
-  *) echo "not support yet"
-     exit 1;;
-esac
-
-ansible-playbook ./playbook-$1.yml -u ${USER} -i "localhost," -c local -v --sudo --vault-password-file=./etc/vault.txt
+ansible-playbook ./playbook.yml -u ${USER} -i "localhost," -c local -v --sudo --vault-password-file=./etc/vault.txt
